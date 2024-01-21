cc79bf45568815ac9adbc1492e9a0b6281c32adb
SathishMadesh <s747272946@gmail.com>
2024-01-21 12:13:02 +0530

    recommender system

---
+++ rec_sys.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction import text
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+data = pd.read_csv("netflixdata.csv")
+print(data.head(10))
+
+data = data[["Title","Description","Content Type","Genres"]]
+print(data.head())
+print(data.isnull().sum())
+
+data = data.dropna()
+print(data)
