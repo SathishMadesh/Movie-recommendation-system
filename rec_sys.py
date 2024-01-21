e40f126fc1391a11b6c1be3b1eaaf7b519d67460
SathishMadesh <s747272946@gmail.com>
2024-01-21 16:52:21 +0530

    update

--- rec_sys.py
+++ rec_sys.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+import re
 
 
 data = pd.read_csv("netflixdata.csv")
